8071a13907d5518d75e6ecb1e447c1e40f9bb1ee
csmali <myurdagul@etu.edu.tr>
2017-12-25 23:26:42 +0300

    added 2 ss

--- pseudo.py
+++ pseudo.py
@@ -5,9 +5,10 @@ import sys
 filename= str(sys.argv[1])
 
 TAB="     "
+DOUBLE_TAB=TAB+TAB
 NEW_LINE= "\n"
-C_PROGRAM_START="import <stdio.h> \n     void main(){ \n"
-C_PROGRAM_END="     }\n"
+C_PROGRAM_START="void main(){ \n"
+C_PROGRAM_END="}\n"
 C_PROGRAM = ""
 lines=[]
 register_dict={}
@@ -17,8 +18,8 @@ C_PROGRAM = C_PROGRAM_START
 i = 0
 file = open(filename, "r") 
 for line in file: 
-	print 'i',i 
-	print line
+	#print 'i',i 
+	#print line
 	line = line.split()
 	
 	# initializing array
@@ -28,7 +29,7 @@ for line in file:
 
 		arrayname=line[0]
 		arraysize=line[3]
-		C_PROGRAM = C_PROGRAM + TAB+TAB+"int "+ arrayname + "["+arraysize+"];"+NEW_LINE
+		C_PROGRAM = C_PROGRAM + TAB+"int "+ arrayname + "["+arraysize+"];"+NEW_LINE
 
 	if line[0]=="lea":
 
@@ -48,13 +49,13 @@ for line in file:
 				if("0x" in line[1].split(',')[1]):
 					register_dict[register]=int(register_dict[register])+int(line[1].split(',')[1][2:],16)
 				else:
-					print register_dict[register]
+					#print register_dict[register]
 					register_index[register]=int(register_index[register])+int(line[1].split(',')[1])/4
 			
 
 		#address change
 		if "[" in line[1].split(',')[0]: 
-			C_PROGRAM = C_PROGRAM + TAB+TAB+register_dict[register]+"["+str(register_index[register])+"]="+str(int(line[1].split(',')[1][2:],16))+";"+NEW_LINE
+			C_PROGRAM = C_PROGRAM + TAB+register_dict[register]+"["+str(register_index[register])+"]="+str(int(line[1].split(',')[1][2:],16))+";"+NEW_LINE
 		#value change
 		else:
 			if("0x" in line[1].split(',')[1]):
@@ -70,7 +71,7 @@ for line in file:
 		elif "e" in  line[1].split(',')[1]:
 			register = line[1].split(',')[0][1:-1]
 			if "[" in line[1].split(',')[0]:
-				C_PROGRAM = C_PROGRAM + TAB+TAB+register_dict[register]+"["+str(register_index[register])+"]="+str(register_values[line[1].split(',')[1]])+";"+NEW_LINE
+				C_PROGRAM = C_PROGRAM + TAB+register_dict[register]+"["+str(register_index[register])+"]="+str(register_values[line[1].split(',')[1]])+";"+NEW_LINE
 		
 	if line[0] == "mul":
 
